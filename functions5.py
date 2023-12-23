2ec280d651a9754804cdef405f4331e8e410076a
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-23 22:35:22 +0800

    Improving

--- functions5.py
+++ functions5.py
@@ -8,6 +8,57 @@ import json
 openai.api_key = open("OpenAI_API_Key.txt", "r").read().strip()
 
 
+
+# --------------------------------------------------------------
+# Use OpenAI’s Function Calling Feature
+# --------------------------------------------------------------
+
+function_descriptions = [
+    {
+        "name": "get_budget",
+        "description": "Get the budget from the user",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "budget_value": {
+                    "type": "integer",
+                    "description": "The budget of the laptop, e.g. 80000",
+                },
+                "currency_symbol": {"type": "string", "enum": ["USD", "INR", "EUR", "GBP", "CAD", "AUD", "JPY", "CNY", "CHF", "SEK", "NZD", 'MYR', "MXN", "SGD", "HKD", "NOK", "KRW", "TRY", "RUB"]},
+                },
+            },
+            "required": ["budget_value", "currency_symbol"],
+        },    
+]
+
+user_prompt = "WEhat is your budget?"
+
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-0613",
+    messages=[{"role": "user", "content": user_prompt}],
+    # Add function calling
+    functions=function_descriptions,
+    function_call="auto",  # specify the function call
+)
+
+# It automatically fills the arguments with correct info based on the prompt
+# Note: the function does not exist yet
+
+output = completion.choices[0].message
+print(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
 def check_budget(response):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0613",
@@ -21,7 +72,7 @@ def check_budget(response):
                     "properties": {
                         "budget_value": {
                             "type": "integer",
-                            "description": "The budget of the laptop, e.g. 80,000 INRT",
+                            "description": "The budget of the laptop, e.g. 80,000",
                         },
                         "currency_symbol": {"type": "string", "enum": ["USD", "INR", "EUR", "GBP", "CAD", "AUD", "JPY", "CNY", "CHF", "SEK", "NZD", 'MYR', "MXN", "SGD", "HKD", "NOK", "KRW", "TRY", "RUB"]},
                     },
@@ -68,4 +119,7 @@ def check_budget(response):
 response = []
 response = check_budget(response)
 print(response)
+
+
+#######
 
