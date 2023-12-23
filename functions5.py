559faa0f73271bae99d0f53c18b5270c46b6d44d
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-23 22:55:27 +0800

    Improving

--- functions5.py
+++ functions5.py
@@ -8,10 +8,52 @@ import json
 openai.api_key = open("OpenAI_API_Key.txt", "r").read().strip()
 
 
-
 # --------------------------------------------------------------
 # Use OpenAI’s Function Calling Feature
 # --------------------------------------------------------------
+
+
+# Define the function descriptions
+function_descriptions = [
+    {
+        "name": "get_budget",
+        "description": "Get the budget from the user",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "budget_value": {
+                    "type": "integer",
+                    "description": "The budget of the laptop, e.g. 80000",
+                },
+                "currency_symbol": {
+                    "type": "string",
+                    "enum": ["USD", "INR", "EUR", "GBP", "CAD", "AUD", "JPY", "CNY", "CHF", "SEK", "NZD", "MYR", "MXN", "SGD", "HKD", "NOK", "KRW", "TRY", "RUB"],
+                },
+            },
+        },
+        "required": ["budget_value", "currency_symbol"],
+    },
+]
+
+# Define the user prompt
+user_prompt = "What is your budget?"
+
+# Make the API call to OpenAI
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-0613",
+    messages=[{"role": "user", "content": user_prompt}],
+    functions=function_descriptions,
+    function_call="auto",
+)
+
+# Extract the assistant's reply from the API response
+assistant_reply = completion.choices[0].message["content"]
+
+# Print the assistant's reply
+print(assistant_reply)
+
+
+"""
 
 function_descriptions = [
     {
@@ -31,7 +73,7 @@ function_descriptions = [
         },    
 ]
 
-user_prompt = "WEhat is your budget?"
+user_prompt = "What is your budget?"
 
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo-0613",
@@ -46,15 +88,4 @@ completion = openai.ChatCompletion.create(
 
 output = completion.choices[0].message
 print(output)
-
-
-
-
-
-
-
-
-
-
-
-
+"""
