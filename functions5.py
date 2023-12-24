43f15f024ecf1fa690ecd7ef5f5f6b5163b86144
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-24 14:56:44 +0800

    Improving

--- functions5.py
+++ functions5.py
@@ -11,6 +11,63 @@ openai.api_key = open("OpenAI_API_Key.txt", "r").read().strip()
 # Ask ChatGPT a Question
 # --------------------------------------------------------------
 
+def get_budget():
+    '''
+    Returns a list [{"role": "system", "content": system_message}]
+    '''
+    
+    delimiter = "####"
+    example_user_req = {'Budget': '80000 INR'}
+
+    
+    system_message = f"""
+
+    You are an intelligent laptop gadget expert and your goal is to find the best laptop for a user.
+    You have already gathered the relevant questions and you already understand the user profile by analysing the user's responses.
+    You final objective is to also get the budget of the user.
+    You want to obtain the following information from the user {'Budget': '80000 INR'}
+    The python dictionary looks like this {{'Budget': 'values'}}
+    The value for 'budget' should be a numerical value extracted from the user's response. 
+    The values currently in the dictionary are only representative values. 
+    
+    {delimiter}Here are some instructions around the values for the different keys. If you do not follow this, you'll be heavily penalised.
+    - The value for 'budget' should be a numerical value extracted from the user's response.
+    - 'Budget' value needs to be greater than or equal to 25000 INR. If the user says less than that, please mention that there are no laptops in that range.
+    - Do not randomly assign values to any of the keys. The values need to be inferred from the user's response.
+    {delimiter}
+
+    To fill the dictionary, you need to have the following chain of thoughts:
+    {delimiter} Thought 1: Ask a question to understand the user's budget  \n
+    If you have received the budget value, then also check if the currency is clear. 
+    If the currency is unclear then ask another question to understand what is the currency.
+    You are trying to fill the following values {{'Budget': 'values'}} in the python dictionary by asking the user.
+    Answer "Yes" or "No" to indicate if you understand the budget and have the right currency. \n
+    If yes, proceed to the next step. Otherwise, rephrase the question to capture the budget and the right currency. \n{delimiter}
+
+    {delimiter}Thought 2: Check if you have correctly updated the values for both the budget and also the right currency in the python dictionary. 
+    If you are not confident about the budget value or the currency , ask clarifying questions. {delimiter}
+
+    Follow the above chain of thoughts and only output the final updated python dictionary. \n
+
+
+    {delimiter} Here is a sample conversation between the user and assistant:
+    Assistant:"Thanks for all the information. Could you kindly let me know your budget for the laptop? You can use any currency that you prefer. This will help me find options that fit within your price range while meeting the specified requirements."
+    User: "My max budget is 80,000"
+    Assistant:"Thanks. Which currency is this?"
+    User: "Indian Rupees. 80,000 indian rupeed."
+    
+    Assistant: "{example_user_req}"
+    {delimiter}
+
+    Thanks the user for providing all the user requirements. And then ask whaht the budget is of the user. Do not start with Assistant: "
+    """
+    conversation = [{"role": "system", "content": system_message}]
+    return conversation
+
+
+
+    
+
 
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo-0613",
