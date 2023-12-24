e73eafb5445e75f873dda6a0c099b10a11e28d2f
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-24 18:56:14 +0800

    Improving

--- app5.py
+++ app5.py
@@ -17,3 +17,23 @@ print(response)
 
 
 
+
+import json
+
+message = {
+    "role": "assistant",
+    "content": None,
+    "function_call": {
+        "name": "get_budget",
+        "arguments": "{\n  \"budget_value\": 80000,\n  \"currency_symbol\": \"INR\"\n}"
+    }
+}
+
+# Extracting budget_value and currency_symbol from the message
+arguments = json.loads(message["function_call"]["arguments"])
+budget_value = arguments["budget_value"]
+currency_symbol = arguments["currency_symbol"]
+
+# Printing the extracted values
+print("budget_value:", budget_value)
+print("currency_symbol:", currency_symbol)
