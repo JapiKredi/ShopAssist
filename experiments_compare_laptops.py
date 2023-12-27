7afeda7472941bc6422b1ad10c666f966a8e8d4b
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-27 17:00:20 +0800

    Improving

--- experiments_compare_laptops.py
+++ experiments_compare_laptops.py
@@ -1,11 +1,10 @@
-
-
+import pandas as pd
 
 laptop_df= pd.read_csv('updated_laptop.csv')
-user_requirements = extract_dictionary_from_string(user_req_string)
-print(currency_conversion_rate)
-budget = int(user_requirements.get('budget', '0').replace(',', '').split()[0])
-    
+budget = 1000
+currency_conversion_rate = 1.200209108
+currency_symbol = 'USD"'
+
 #This line retrieves the value associated with the key 'budget' from the user_requirements dictionary.
 #If the key is not found, the default value '0' is used.
 #The value is then processed to remove commas, split it into a list of strings, and take the first element of the list.
@@ -15,5 +14,7 @@ budget = int(user_requirements.get('budget', '0').replace(',', '').split()[0])
 filtered_laptops = laptop_df.copy()
 filtered_laptops['Price'] = filtered_laptops['Price'].str.replace(',','').astype(int)
 filtered_laptops['Price'] = filtered_laptops['Price'] * currency_conversion_rate
+filtered_laptops['Currency'] = currency_symbol
+
 filtered_laptops = filtered_laptops[filtered_laptops['Price'] <= budget].copy()
 print(filtered_laptops)
